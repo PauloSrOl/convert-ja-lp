cd2d13f44b0ceaa615e31fcd0cebca930429fa6c
Paulo Soares <paulosr870@gmail.com>
2024-03-30 09:24:58 -0300

    animação dos cards

---
+++ animacoes.js
@@ -0,0 +1,36 @@
+var imagem = document.querySelector("#imagem");
+var card1 = document.querySelector("#card1")
+var card2 = document.querySelector("#card2")
+var card3 = document.querySelector("#card3")
+
+
+card1.addEventListener('mouseover', function(){
+    document.getElementById("imagem1").src="../imagens/Logo-pp-roxo.png"
+  
+});
+
+card1.addEventListener('mouseout', function(){
+    document.getElementById("imagem1").src="../imagens/logo-pp.png"
+});
+
+
+
+card2.addEventListener('mouseover', function(){
+    document.getElementById("imagem2").src="../imagens/Logo-pp-roxo.png"
+});
+
+card2.addEventListener('mouseout', function(){
+    document.getElementById("imagem2").src="../imagens/logo-pp.png"
+});
+
+
+
+card3.addEventListener('mouseover', function(){
+    document.getElementById("imagem3").src="../imagens/Logo-pp-roxo.png"
+});
+
+card3.addEventListener('mouseout', function(){
+    document.getElementById("imagem3").src="../imagens/logo-pp.png"
+});
+
+
